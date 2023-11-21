421c889b09706cb51a7f7e4d2d132fad6b55f083
david.mise <davidkingmushi@gmail.com>
2023-11-21 23:06:31 +0300

    added the sign in button

--- lib/ui/home.dart
+++ lib/ui/home.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+// import 'package:firebase_auth/firebase_auth.dart';
+// import 'package:google_sign_in/google_sign_in.dart';
+// import 'package:flutter_signin_button/flutter_signin_button.dart';
+
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -21,7 +25,6 @@ class _HomeState extends State<Home> {
             child: Row(
               mainAxisAlignment: MainAxisAlignment.center,
               // crossAxisAlignment: CrossAxisAlignment.center,
-
               children: [
                 Text(
                   'Virtual',
@@ -37,53 +40,120 @@ class _HomeState extends State<Home> {
               ],
             ),
           ),
-          Container(
-            child: const Text(
-              'Sign in',
-              style: TextStyle(
-                fontSize: 28,
-                color: Colors.white70,
-              ),
+          const Text(
+            'Sign in',
+            style: TextStyle(
+              fontSize: 28,
+              color: Colors.white70,
             ),
           ),
-          const Padding(
-            padding: EdgeInsets.all(10),
+          Padding(
+            padding: const EdgeInsets.all(10),
             child: Form(
               child: Column(
                 children: [
-
-                  TextField(
-                    keyboardType:TextInputType.emailAddress,
+                  const TextField(
+                    keyboardType: TextInputType.emailAddress,
                     decoration: InputDecoration(
-                        fillColor: Color.fromRGBO(223, 208, 178, 1),
-                        filled: true,
-                        labelText: 'Email',
-                        // border: OutlineInputBorder(),
-                        focusedBorder: OutlineInputBorder(
-                          borderSide: BorderSide(color:Colors.black),
-                        ),
-                        prefixIcon: Icon(Icons.mark_email_unread_rounded)
+                      fillColor: Color.fromRGBO(223, 208, 178, 1),
+                      filled: true,
+                      labelText: 'Email or phone',
+                      // border: OutlineInputBorder(),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.white),
+                      ),
+                      prefixIcon: Icon(Icons.mark_email_unread_rounded,
+                          color: Colors.black),
                     ),
                   ),
-                  SizedBox(height:20),
-                  TextField(
-                    keyboardType:TextInputType.emailAddress,
+                  const SizedBox(height: 20),
+                  const TextField(
+                    keyboardType: TextInputType.emailAddress,
                     decoration: InputDecoration(
-                        fillColor: Color.fromRGBO(223, 208, 178, 1),
-                        filled: true,
-                        labelText: 'Password',
-                        border: OutlineInputBorder(borderSide: BorderSide(color:Colors.black),),
-                        // enabledBorder: OutlineInputBorder(borderSide: BorderSide(color:Colors.black),),
-                        prefixIcon: Icon(Icons.password_rounded)
+                      fillColor: Color.fromRGBO(223, 208, 178, 1),
+                      filled: true,
+                      labelText: 'Password',
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.white),
+                      ),
+                      // enabledBorder: OutlineInputBorder(borderSide: BorderSide(color:Colors.black),),
+                      prefixIcon:
+                          Icon(Icons.password_rounded, color: Colors.black),
+                    ),
+                  ),
+                  const SizedBox(height: 25.0),
+                  SizedBox(
+                    width: 150.0,
+                    height: 50.0,
+                    child: ElevatedButton(
+                      style: ButtonStyle(
+                        backgroundColor: MaterialStateProperty.all<Color>(
+                            Colors.brown.shade900),
+                        shape: MaterialStateProperty.all<OutlinedBorder>(
+                          RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(25),
+                          ),
+                        ),
+                      ),
+                      onPressed: () {},
+                      child: const Text(
+                        'Sign in',
+                        style: TextStyle(fontSize: 20),
+                      ),
                     ),
                   ),
                 ],
               ),
             ),
           ),
+          const SizedBox(height: 20.0),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 10),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                GestureDetector(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => const Home()),
+                    );
+                  },
+                  child: const Text(
+                    ' Don\'t have an account? ',
+                    style: TextStyle(fontSize: 16.0, color: Colors.white),
+                  ),
+                ),
+
+                const SizedBox(
+                  width: 20.0,
+                ),
+                GestureDetector(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => const Home()),
+                    );
+                  },
+                  child: const Text(
+                    'Forgot Password?',
+                    style: TextStyle(
+                      fontSize: 16,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+
         ],
       ),
     );
-
   }
 }
+
+
+
+
+
