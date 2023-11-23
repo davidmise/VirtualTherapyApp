b6f65a16225b725b137b264b8d1f97e2d202c5bf
david.mise <davidkingmushi@gmail.com>
2023-11-23 10:54:57 +0300

    added a ScrollView in register Page

--- lib/ui/register.dart
+++ lib/ui/register.dart
@@ -16,207 +16,209 @@ class _RegisterState extends State<Register> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey[800],
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Padding(
-              padding: EdgeInsets.all(10),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                // crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Text(
-                    'Virtual',
-                    style: TextStyle(fontSize: 30, color: Colors.redAccent),
-                  ),
-                  Text(
-                    'Therapy',
-                    style: TextStyle(
-                      fontSize: 30,
-                      color: Colors.yellowAccent,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16.0,vertical: 130.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Padding(
+                padding: EdgeInsets.all(10),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  // crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text(
+                      'Virtual',
+                      style: TextStyle(fontSize: 30, color: Colors.redAccent),
                     ),
-                  ),
-                ],
+                    Text(
+                      'Therapy',
+                      style: TextStyle(
+                        fontSize: 30,
+                        color: Colors.yellowAccent,
+                      ),
+                    ),
+                  ],
+                ),
               ),
-            ),
-            const Text(
-              'Create an account',
-              style: TextStyle(
-                fontSize: 28,
-                color: Colors.white70,
+              const Text(
+                'Create an account',
+                style: TextStyle(
+                  fontSize: 28,
+                  color: Colors.white70,
+                ),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(10),
-              child: Form(
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 10),
-                  child: Column(
-                    children: [
-                      const TextField(
-                        keyboardType: TextInputType.emailAddress,
-                        decoration: InputDecoration(
-                          fillColor: Color.fromRGBO(223, 208, 178, 1),
-                          filled: true,
-                          labelText: 'Username',
-                          // border: OutlineInputBorder(),
-                          focusedBorder: OutlineInputBorder(
-                            borderSide: BorderSide(color: Colors.white),
-                          ),
-                          prefixIcon:
-                              Icon(Icons.person_2_rounded, color: Colors.black),
-                        ),
-                      ),
-                      const SizedBox(height: 20),
-                      const TextField(
-                        keyboardType: TextInputType.emailAddress,
-                        decoration: InputDecoration(
-                          fillColor: Color.fromRGBO(223, 208, 178, 1),
-                          filled: true,
-                          labelText: 'Email or phone',
-                          // border: OutlineInputBorder(),
-                          focusedBorder: OutlineInputBorder(
-                            borderSide: BorderSide(color: Colors.white),
-                          ),
-                          prefixIcon: Icon(Icons.mark_email_unread_rounded,
-                              color: Colors.black),
-                        ),
-                      ),
-                      const SizedBox(height: 20),
-                      TextField(
-                        obscureText: _obscureText,
-                        keyboardType: TextInputType.emailAddress,
-                        decoration: InputDecoration(
-                          fillColor: const Color.fromRGBO(223, 208, 178, 1),
-                          filled: true,
-                          labelText: 'Password',
-                          focusedBorder: const OutlineInputBorder(
-                            borderSide: BorderSide(color: Colors.white),
-                          ),
-                          // enabledBorder: OutlineInputBorder(borderSide: BorderSide(color:Colors.black),),
-
-                          suffixIcon: IconButton(
-                            icon: Icon(
-                              _obscureText
-                                  ? Icons.visibility
-                                  : Icons.visibility_off,
-                              color: Colors.black,
+              Padding(
+                padding: const EdgeInsets.all(10),
+                child: Form(
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 10),
+                    child: Column(
+                      children: [
+                        const TextField(
+                          keyboardType: TextInputType.emailAddress,
+                          decoration: InputDecoration(
+                            fillColor: Color.fromRGBO(223, 208, 178, 1),
+                            filled: true,
+                            labelText: 'Username',
+                            // border: OutlineInputBorder(),
+                            focusedBorder: OutlineInputBorder(
+                              borderSide: BorderSide(color: Colors.white),
                             ),
-                            onPressed: () {
-                              setState(() {
-                                _obscureText = !_obscureText;
-                              });
-                            },
+                            prefixIcon: Icon(Icons.person_2_rounded,
+                                color: Colors.black),
                           ),
                         ),
-                      ),
-                      const SizedBox(height: 20),
-                      TextField(
-                        obscureText: _obscureText2,
-                        keyboardType: TextInputType.emailAddress,
-                        decoration: InputDecoration(
-                          fillColor: const Color.fromRGBO(223, 208, 178, 1),
-                          filled: true,
-                          labelText: 'Confirm Password',
-                          focusedBorder: const OutlineInputBorder(
-                            borderSide: BorderSide(color: Colors.white),
-                          ),
-                          // enabledBorder: OutlineInputBorder(borderSide: BorderSide(color:Colors.black),),
-
-                          suffixIcon: IconButton(
-                            icon: Icon(
-                              _obscureText2
-                                  ? Icons.visibility
-                                  : Icons.visibility_off,
-                              color: Colors.black,
+                        const SizedBox(height: 20),
+                        const TextField(
+                          keyboardType: TextInputType.emailAddress,
+                          decoration: InputDecoration(
+                            fillColor: Color.fromRGBO(223, 208, 178, 1),
+                            filled: true,
+                            labelText: 'Email or phone',
+                            // border: OutlineInputBorder(),
+                            focusedBorder: OutlineInputBorder(
+                              borderSide: BorderSide(color: Colors.white),
                             ),
-                            onPressed: () {
-                              setState(() {
-                                _obscureText2 = !_obscureText2;
-                              });
-                            },
+                            prefixIcon: Icon(Icons.mark_email_unread_rounded,
+                                color: Colors.black),
                           ),
                         ),
-                      ),
-                      const SizedBox(height: 25.0),
-                      SizedBox(
-                        width: 150.0,
-                        height: 50.0,
-                        child: ElevatedButton(
-                          style: ButtonStyle(
-                            backgroundColor: MaterialStateProperty.all<Color>(
-                                Colors.brown.shade900),
-                            shape: MaterialStateProperty.all<OutlinedBorder>(
-                              RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(25),
+                        const SizedBox(height: 20),
+                        TextField(
+                          obscureText: _obscureText,
+                          keyboardType: TextInputType.emailAddress,
+                          decoration: InputDecoration(
+                            fillColor: const Color.fromRGBO(223, 208, 178, 1),
+                            filled: true,
+                            labelText: 'Password',
+                            focusedBorder: const OutlineInputBorder(
+                              borderSide: BorderSide(color: Colors.white),
+                            ),
+                            // enabledBorder: OutlineInputBorder(borderSide: BorderSide(color:Colors.black),),
+
+                            suffixIcon: IconButton(
+                              icon: Icon(
+                                _obscureText
+                                    ? Icons.visibility
+                                    : Icons.visibility_off,
+                                color: Colors.black,
+                              ),
+                              onPressed: () {
+                                setState(() {
+                                  _obscureText = !_obscureText;
+                                });
+                              },
+                            ),
+                          ),
+                        ),
+                        const SizedBox(height: 20),
+                        TextField(
+                          obscureText: _obscureText2,
+                          keyboardType: TextInputType.emailAddress,
+                          decoration: InputDecoration(
+                            fillColor: const Color.fromRGBO(223, 208, 178, 1),
+                            filled: true,
+                            labelText: 'Confirm Password',
+                            focusedBorder: const OutlineInputBorder(
+                              borderSide: BorderSide(color: Colors.white),
+                            ),
+                            // enabledBorder: OutlineInputBorder(borderSide: BorderSide(color:Colors.black),),
+
+                            suffixIcon: IconButton(
+                              icon: Icon(
+                                _obscureText2
+                                    ? Icons.visibility
+                                    : Icons.visibility_off,
+                                color: Colors.black,
+                              ),
+                              onPressed: () {
+                                setState(() {
+                                  _obscureText2 = !_obscureText2;
+                                });
+                              },
+                            ),
+                          ),
+                        ),
+                        const SizedBox(height: 25.0),
+                        SizedBox(
+                          width: 150.0,
+                          height: 50.0,
+                          child: ElevatedButton(
+                            style: ButtonStyle(
+                              backgroundColor: MaterialStateProperty.all<Color>(
+                                  Colors.brown.shade900),
+                              shape: MaterialStateProperty.all<OutlinedBorder>(
+                                RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(25),
+                                ),
                               ),
                             ),
-                          ),
-                          onPressed: () {},
-                          child: const Text(
-                            'Sign Up',
-                            style: TextStyle(fontSize: 20),
+                            onPressed: () {},
+                            child: const Text(
+                              'Sign Up',
+                              style: TextStyle(fontSize: 20),
+                            ),
                           ),
                         ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 20.0),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 10),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    GestureDetector(
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => const Home()),
+                        );
+                      },
+                      child: const Text(
+                        ' I have an account ',
+                        style: TextStyle(fontSize: 16.0, color: Colors.white),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(height: 25.0),
+              SizedBox(
+                width: 250.0,
+                height: 50.0,
+                child: ElevatedButton(
+                  onPressed: () {},
+                  style: const ButtonStyle(
+                    backgroundColor: MaterialStatePropertyAll(Colors.white),
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Container(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Image.asset(
+                          'images/G.png', // Replace with the actual path to your Google logo image
+                          height: 25.0,
+                          width: 25.0, // Adjust the height as needed
+                        ),
+                      ),
+                      const Text(
+                        'Sign in with Google',
+                        style: TextStyle(color: Colors.black, fontSize: 17),
                       ),
                     ],
                   ),
                 ),
               ),
-            ),
-            const SizedBox(height: 20.0),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 10),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  GestureDetector(
-                    onTap: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => const Home()),
-                      );
-                    },
-                    child: const Text(
-                      ' I have an account ',
-                      style: TextStyle(fontSize: 16.0, color: Colors.white),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            const SizedBox(height: 25.0),
-            SizedBox(
-              width: 250.0,
-              height: 50.0,
-              child: ElevatedButton(
-                onPressed: () {},
-                style: const ButtonStyle(
-                  backgroundColor: MaterialStatePropertyAll(Colors.white),
-                ),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Container(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Image.asset(
-                        'images/G.png', // Replace with the actual path to your Google logo image
-                        height: 25.0,
-                        width: 25.0, // Adjust the height as needed
-                      ),
-                    ),
-                    const Text(
-                      'Sign in with Google',
-                      style: TextStyle(color: Colors.black, fontSize: 17),
-                    ),
-                  ],
-                ),
-              ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
